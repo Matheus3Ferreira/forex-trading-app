28b62ca9142b9e4d86d30fa50648cebe33abb8c7
Matheus Ferreira <ferreiromagrelo@gmail.com>
2022-06-30 12:40:10 -0300

    feat: Sign In page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SignUpModal from './components/SignUpModal/SignUpModal';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from "react-router-dom";
+import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
+import Welcome from './pages/Welcome/Welcome';
 
 function App() {
+
   return (
-    <div>
-      <SignUpModal />
-    </div>
+    <BrowserRouter> 
+    <Routes>
+      <Route index element={<Welcome />} />
+      <Route path="sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+   </BrowserRouter>
   );
 }
 
