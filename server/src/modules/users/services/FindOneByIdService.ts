ef85a81d27e905852a83775e7a887e25f06560b9
Matheus Ferreira <ferreiromagrelo@gmail.com>
2022-07-05 08:17:17 -0300

    fix: Remove search by email to id

---
+++ server/src/modules/users/services/FindOneByIdService.ts
@@ -0,0 +1,14 @@
+import User from "../models/user";
+import { Types } from "mongoose";
+import IUser from "./IUserService";
+
+export default class findOneByIdService {
+  public async execute(id: string): Promise<IUser> {
+    if (!Types.ObjectId.isValid(id)) throw new Error("Invalid id");
+
+    const user: IUser | null = await User.findById(id);
+    if (!user) throw new Error("User not found");
+
+    return user;
+  }
+}
